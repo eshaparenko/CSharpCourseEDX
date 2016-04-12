1ac0dd0afa459643da365cbe2a6a8cc8d683add7
eugene <gesha8610@gmail.com>
2016-04-12 18:30:47 +0300

    EDX First assignment - DONE

--- EDX_Classes/Program.cs
+++ EDX_Classes/Program.cs
@@ -10,6 +10,23 @@ namespace EDX_Classes
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What is the student's first name?");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("What is the student's last name?");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("What is the student's birthdate?");
+            string birthdtae = Console.ReadLine();
+            Console.WriteLine("What is the student's addressLine1?");
+            string addressLine1 = Console.ReadLine();
+            Console.WriteLine("What is the student's addressLine2?");
+            string addressLine2 = Console.ReadLine();
+            Console.WriteLine("What city does student live?");
+            string city = Console.ReadLine();
+            Console.WriteLine("What is the student's postal number?");
+            int postal = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("What country does student live?");
+            string country = Console.ReadLine();
+
         }
     }
 }
