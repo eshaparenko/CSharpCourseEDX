51ad6efc10e1fcbfec4702e8ee94a4b7320df8ef
eugene <gesha8610@gmail.com>
2016-04-13 12:52:11 +0300

    EDX Second assignment - Done

---
+++ ChessBoard/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessBoard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                
+                    if (i % 2 == 0)
+                    {
+                        for (int j = 0; j < 4; j++)
+                        {
+                            Console.Write("XO");
+                            
+                        }
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        for (int j = 0; j < 4; j++)
+                        {
+                            Console.Write("OX");
+                            
+                        }
+                        Console.WriteLine();
+                    }
+                    
+                
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
